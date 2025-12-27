9d244ef4f4462ac556ea4f9e25bab5ae55aee573
Joseph R <raczynski.joseph616@gmail.com>
2025-12-27 14:28:30 -0600

    Add code for BetterStudentManager.java

--- src/main/java/org/example/BetterStudentManager.java
+++ src/main/java/org/example/BetterStudentManager.java
@@ -12,23 +12,30 @@ public class BetterStudentManager {
 
     public void addStudent(String name, double grade) {
         // This method should add a new student to the list of students
+        Student student = new Student();
+        student.name = name;
+        student.grade = grade;
+        students.add(student);
     }
 
     public void removeStudent(String name) {
-        // This method should remove the student with the given name from the list of students
+        for (int i = 0; i < students.size(); i++) {
+            Student student = students.get(i);
+            if (student.name.equals(name)) {
+                students.remove(i);
+                break;
+            }
+        }
     }
 
     public String getStudentList() {
-        // This method should return a string with each student's name and grade
-        // separated by a space, with each student separated by a newline character ('\n')
-        // E.g.
-        // John 90.9
-        // Jane 88.3
-        //
-        // You will need to use a String or StringBuilder to build the result by
-        // looping through the list of students
-        //
-        // Replace the following line with your implementation
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (Student student : students) {
+            sb.append(student.name);
+            sb.append(" ");
+            sb.append(student.grade);
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 }
